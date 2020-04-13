0b1797b8885c7cc5672905f1f514fb33c239d245
Athanasios Raptodimos <thanosra@gmail.com>
2020-04-14 00:31:14 +0300

    Add data loader

---
+++ src/main/java/eu/kalodiodev/controlmycar/bootstrap/DataLoader.java
@@ -0,0 +1,67 @@
+package eu.kalodiodev.controlmycar.bootstrap;
+
+import eu.kalodiodev.controlmycar.converter.CarToCarCommand;
+import eu.kalodiodev.controlmycar.domains.Car;
+import eu.kalodiodev.controlmycar.domains.Role;
+import eu.kalodiodev.controlmycar.domains.User;
+import eu.kalodiodev.controlmycar.services.CarService;
+import eu.kalodiodev.controlmycar.services.RoleService;
+import eu.kalodiodev.controlmycar.services.UserService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements CommandLineRunner {
+
+    private final RoleService roleService;
+    private final UserService userService;
+    private final CarService carService;
+    private final CarToCarCommand carToCarCommand;
+
+    public DataLoader(RoleService roleService, UserService userService, CarService carService, CarToCarCommand carToCarCommand) {
+        this.roleService = roleService;
+        this.userService = userService;
+        this.carService = carService;
+        this.carToCarCommand = carToCarCommand;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        // Add Roles
+        Role adminRole = new Role();
+        adminRole.setName("ADMIN");
+        roleService.save(adminRole);
+
+        Role userRole = new Role();
+        userRole.setName("USER");
+        roleService.save(userRole);
+
+        // Add Users
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String secret = encoder.encode("password");
+
+        User user = new User();
+        user.setEmail("user@example.com");
+        user.setPassword(secret);
+        user.setFirstName("John");
+        user.setLastName("Doe");
+        user.addRole(userRole);
+
+        userService.save(user);
+
+        // Add Cars
+        Car car = new Car();
+        car.setNumberPlate("AAA-1234");
+        car.setManufacturer("Nissan");
+        car.setModel("Qashqai");
+        car.setManufacturedYear(2009);
+        car.setOwnedYear(2009);
+        car.setInitialOdometer(10d);
+        car.setBoughtPrice(33100d);
+        car.setUser(user);
+
+        carService.save(carToCarCommand.convert(car));
+    }
+}
