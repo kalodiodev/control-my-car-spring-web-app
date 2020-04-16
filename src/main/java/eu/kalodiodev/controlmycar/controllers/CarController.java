850803467874832d824f76ae6a0bcef492e13d0f
Athanasios Raptodimos <thanosra@gmail.com>
2020-04-17 01:02:39 +0300

    Add post new car

--- src/main/java/eu/kalodiodev/controlmycar/controllers/CarController.java
+++ src/main/java/eu/kalodiodev/controlmycar/controllers/CarController.java
@@ -3,9 +3,9 @@ package eu.kalodiodev.controlmycar.controllers;
 import eu.kalodiodev.controlmycar.command.CarCommand;
 import eu.kalodiodev.controlmycar.converter.CarToCarCommand;
 import eu.kalodiodev.controlmycar.services.CarService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class CarController {
@@ -21,5 +21,12 @@ public class CarController {
     @GetMapping("/users/{userId}/cars/{carId}")
     public CarCommand findCar(@PathVariable Long userId, @PathVariable Long carId) {
         return carToCarCommand.convert(carService.findByUserIdAndCarId(userId, carId));
+    }
+
+    @PostMapping("/users/{userId}/cars")
+    public ResponseEntity<CarCommand> addCar(@PathVariable Long userId, @RequestBody CarCommand command) {
+        command.setUserId(userId);
+
+        return new ResponseEntity<>(carToCarCommand.convert(carService.save(command)), HttpStatus.CREATED);
     }
 }
--- src/test/java/eu/kalodiodev/controlmycar/controllers/CarControllerTest.java
+++ src/test/java/eu/kalodiodev/controlmycar/controllers/CarControllerTest.java
@@ -1,5 +1,6 @@
 package eu.kalodiodev.controlmycar.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import eu.kalodiodev.controlmycar.command.CarCommand;
 import eu.kalodiodev.controlmycar.converter.CarToCarCommand;
 import eu.kalodiodev.controlmycar.domains.Car;
@@ -10,12 +11,16 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -29,6 +34,8 @@ public class CarControllerTest {
 
     @InjectMocks
     CarController controller;
+
+    private static final ObjectMapper om = new ObjectMapper();
 
     MockMvc mockMvc;
 
@@ -53,5 +60,27 @@ public class CarControllerTest {
         mockMvc.perform(get("/users/1/cars/1"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id", is(1)));
+    }
+
+    @Test
+    void save_car() throws Exception {
+
+        CarCommand carCommand = new CarCommand();
+        carCommand.setManufacturer("Nissan");
+        carCommand.setModel("Micra");
+        carCommand.setBoughtPrice(10000d);
+        carCommand.setInitialOdometer(1000d);
+        carCommand.setManufacturedYear(2009);
+        carCommand.setOwnedYear(2010);
+        carCommand.setNumberPlate("AAA-1234");
+
+        when(carService.save(any(CarCommand.class))).thenReturn(new Car());
+        when(carToCarCommand.convert(any(Car.class))).thenReturn(carCommand);
+
+        mockMvc.perform(post("/users/1/cars")
+                .content(om.writeValueAsString(carCommand))
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.model", is("Micra")));
     }
 }
